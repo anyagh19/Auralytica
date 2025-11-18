3e9b1cd1dcbbf5b8deb6361defb65656c06e9acb
anyagh19 <aniketmarkad1509@gmail.com>
2025-11-18 11:39:34 +0530

    cors error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,6 +147,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://auralytica.onrender.com"
+    "https://auralytica.onrender.com",
+    "https://auralytica.vercel.app/"
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
